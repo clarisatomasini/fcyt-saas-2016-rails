7f40287b1f8f77ab7bb7b012a9537dd227184b69
Jorge <jorgechiavaro@gmail.com>
2016-08-27 23:53:09 +0000

    Add rails app for homeworks

---
+++ rails-coursera/app/controllers/courses_controller.rb
@@ -0,0 +1,17 @@
+class CoursesController < ApplicationController
+    def index
+        #CHANGE THIS TO SORT BY TITLE ASC
+        #Instead of call Course.all, create a method in the model
+        #Course.find_all_sorted
+        @courses = Course.all
+    end
+    def new
+    end
+    def create
+        @course = Course.new(params.require(:course).permit(:title, :description, :institution))
+        #YOUR CODE HERE
+    end
+    def show
+        #YOUR CODE HERE
+    end
+end
---
+++ rails-coursera/spec/models/course_model_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe Course, :type => :model do
+    it "creates a new course" do
+        ia = Course.create!(title: "Artificial Intelligence", description: "IA intro", institution: "FCYT")
+
+        expect(ia).not_to be nil
+        expect(ia.title).to eql "Artificial Intelligence"
+    end
+    xit "Gets sopecific course by id" do
+        #YOUR CODE HERE
+    end
+    xit "Lists all course sorted by title asc" do
+        #YOUR CODE HERE
+    end
+end
